53b46216b3d20ac77142afc12c216022895c858a
Leigh Richards <73237857+Winifreddo@users.noreply.github.com>
2024-06-26 10:11:40 +0100

    set up imagery/local fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,21 @@
 import Image from "next/image";
 import prisma from "@/lib/prisma";
-
+import "/public/fonts/fonts.css";
 // async function getUser() {
 //   const user = await prisma.user.findMany({
 //     where: { email: "100983uk@saeinstitute.edu" },
 //   });
 //   return user;
 // }
-import "tailwindcss/tailwind.css";
+// import "tailwindcss/tailwind.css";
 
 export default async function Home() {
   // const users = await getUser();
   // console.log(users);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hello
+    <main className="flex min-h-screen flex-col items-center justify-between text-pink-600 p-24 font-sans">
+      <h1 className="Title"> Hello</h1>
+      <Image src="/images/shorts1.webp" alt="logo" width={200} height={200} />
     </main>
   );
 }
