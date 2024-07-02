ce0bf2696d9cfad128edec560e070314b3d46029
Leigh Richards <73237857+Winifreddo@users.noreply.github.com>
2024-07-02 14:08:49 +0100

    adjusted responsiveness

--- app/FAQs/page.tsx
+++ app/FAQs/page.tsx
@@ -9,15 +9,15 @@ const Page = () => {
         <h2 className="w-full text-stoneMedium text-center text-7xl font-poppins font-bold">
           ABOUT{" "}
         </h2>
-        <h1 className="text-stoneMedium text-xl p-2">RE-FORM</h1>
+        <h1 className="text-stoneMedium text-xl p-2 md:flex hidden">RE-FORM</h1>
         <h2 className="w-full text-stoneMedium text-center text-7xl font-poppins font-bold">
           {" "}
           US
         </h2>
       </header>
 
-      <section className="grid md:grid-cols-2 grid-cols-1 w-full h-full place-items-center text-center">
-        <div className="flex  items-center justify-center bg-mossGreen text-stoneDark p-16 h-screen w-full ">
+      <section className="grid md:grid-cols-2 grid-cols-1 w-full md:h-full place-items-center text-center">
+        <div className="flex md:flex-row flex-col items-center justify-center bg-mossGreen text-stoneDark p-16 md:h-screen w-full ">
           <Image
             src="/images/shirtTrans.svg"
             alt="About us"
@@ -26,8 +26,8 @@ const Page = () => {
           />
         </div>
 
-        <div className="bg-stoneDark text-mossGreen w-full p-16 h-screen flex flex-col items-center justify-center">
-          <p className=" leading-loose p-12  text-3xl font-poppins font-thin">
+        <div className="bg-stoneDark text-mossGreen w-full p-16 md:h-screen flex flex-col items-center justify-center">
+          <p className=" leading-loose md:p-12 p-6 text-3xl font-poppins font-thin">
             Fashion is <b>Fun</b>. <br />
             Fashion is <b>Empowering</b>. <br />
             Fashion is <b>Art</b>. <br />
@@ -35,11 +35,11 @@ const Page = () => {
             Style is <b>Personal</b>. <br />
           </p>
         </div>
-        <div className="bg-stoneDark text-mossGreen w-full h-screen p-16 flex flex-col items-center justify-center">
-          <h3 className="font-poppins font-bold text-center text-5xl">
+        <div className="bg-stoneDark text-mossGreen w-full md:h-screen p-16 flex flex-col items-center justify-center">
+          <h3 className="font-poppins font-bold text-center md:text-5xl text-4xl pb-3">
             WHO WE ARE
           </h3>
-          <p className=" leading-loose py-4 px-16 font-poppins text-xl font-thin">
+          <p className=" leading-loose py-4 md:px-16  px-2 font-poppins text-xl font-thin">
             We are a brand who love fashion and the planet! We are committed to
             creating a brand where the clothes are made with love and care for
             the environment. We believe that fashion should be accessible
@@ -48,7 +48,7 @@ const Page = () => {
             more responsible fashion future.
           </p>
         </div>
-        <div className="bg-darkPink w-full h-screen p-16 flex items-center justify-center">
+        <div className="bg-darkPink w-full md:h-screen p-16 flex items-center justify-center">
           <Image
             src="/images/earringsTrans.svg"
             alt="About us"
@@ -59,7 +59,7 @@ const Page = () => {
       </section>
       <section>
         <div className="flex flex-col items-center justify-center bg-smokeGrey text-stoneDark p-12 w-full ">
-          <p className=" leading-loose py-4 px-16 font-poppins text-2xl font-thin">
+          <p className=" leading-loose py-4 md:px-16 px-2 font-poppins text-2xl font-thin">
             <span className="F">
               <i>F</i>
             </span>
@@ -69,8 +69,8 @@ const Page = () => {
           </p>
         </div>
       </section>
-      <section className="grid md:grid-cols-2 grid-cols-1 w-full h-full place-items-center text-center">
-        <div className="flex  items-center justify-center bg-mossGreen text-stoneDark p-16 h-screen w-full ">
+      <section className="grid md:grid-cols-2 grid-cols-1 w-full md:h-full place-items-center text-center">
+        <div className="flex  items-center justify-center bg-mossGreen text-stoneDark p-16 md:h-screen w-full ">
           <Image
             src="/images/tankTrans.svg"
             alt="About us"
@@ -79,29 +79,29 @@ const Page = () => {
           />
         </div>
 
-        <div className="bg-stoneDark text-mossGreen w-full p-16 h-screen flex flex-col items-center justify-center">
-          <h3 className="font-poppins font-bold text-center text-5xl">
+        <div className="bg-stoneDark text-mossGreen w-full p-16 md:h-screen flex flex-col items-center justify-center">
+          <h3 className="font-poppins font-bold text-center md:text-5xl text-4xl pb-3">
             SUSTAINABILITY
           </h3>
-          <p className=" leading-loose p-12  text-xl font-poppins font-thin">
+          <p className=" leading-loose py-4 md:px-16 px-2  text-xl font-poppins font-thin">
             Every piece we design is crafted with care, using sustainable
             materials and processes that minimize our ecological footprint. From
             organic fabrics to recycled packaging, we are dedicated to
             protecting our planet for future generations.
           </p>
         </div>
-        <div className="bg-stoneDark text-mossGreen w-full h-screen p-16 flex flex-col items-center justify-center">
-          <h3 className="font-poppins font-bold text-center text-5xl">
+        <div className="bg-stoneDark text-mossGreen w-full md:h-screen p-16 flex flex-col items-center justify-center">
+          <h3 className="font-poppins font-bold text-center md:text-5xl text-4xl pb-3">
             ETHICAL PRACTICES
           </h3>
-          <p className=" leading-loose py-4 px-16 font-poppins text-xl font-thin">
+          <p className=" leading-loose py-4 md:px-16 px-2 font-poppins text-xl font-thin">
             We value transparency and fairness in every step of our supply
             chain. Our commitment to ethical practices ensures that all
             individuals involved in the creation of our products are treated
             with dignity, paid fairly, and work in safe conditions.
           </p>
         </div>
-        <div className="bg-darkPink w-full h-screen p-16 flex items-center justify-center">
+        <div className="bg-darkPink w-full md:h-screen p-16 flex items-center justify-center">
           <Image
             src="/images/jacketTrans.svg"
             alt="About us"
@@ -109,7 +109,7 @@ const Page = () => {
             height={350}
           />
         </div>
-        <div className="flex  items-center justify-center bg-mossGreen text-stoneDark p-16 h-screen w-full ">
+        <div className="flex items-center justify-center bg-mossGreen text-stoneDark p-16 md:h-screen w-full ">
           <Image
             src="/images/hairclipTrans.svg"
             alt="About us"
@@ -118,27 +118,27 @@ const Page = () => {
           />
         </div>
 
-        <div className="bg-stoneDark text-mossGreen w-full p-16 h-screen flex flex-col items-center justify-center">
-          <h3 className="font-poppins font-bold text-center text-5xl">
+        <div className="bg-stoneDark text-mossGreen w-full p-16 md:h-screen flex flex-col items-center justify-center">
+          <h3 className="font-poppins font-bold text-center md:text-5xl text-4xl pb-3">
             TIMELESS STYLE
           </h3>
-          <p className=" leading-loose p-12  text-xl font-poppins font-thin">
+          <p className=" leading-loose py-4 md:px-16 px-2 text-xl font-poppins font-thin">
             We believe that true fashion transcends trends. Our designs are
             timeless, versatile, and made to last. By creating pieces that
             endure, we encourage mindful consumption and reduce waste.
           </p>
         </div>
-        <div className="bg-stoneDark text-mossGreen w-full h-screen p-16 flex flex-col items-center justify-center">
-          <h3 className="font-poppins font-bold text-center text-5xl">
+        <div className="bg-stoneDark text-mossGreen w-full md:h-screen p-16 flex flex-col items-center justify-center">
+          <h3 className="font-poppins font-bold text-center md:text-5xl text-4xl pb-3">
             INNOVATION
           </h3>
-          <p className=" leading-loose py-4 px-16 font-poppins text-xl font-thin">
+          <p className=" leading-loose py-4 md:px-16 px-2 font-poppins text-xl font-thin">
             We are constantly exploring new ways to improve our sustainability
             practices. Through innovative design and forward-thinking solutions,
             we strive to set new standards in the fashion industry.
           </p>
         </div>
-        <div className="bg-darkPink w-full h-screen p-16 flex items-center justify-center">
+        <div className="bg-darkPink w-full md:h-screen p-16 flex items-center justify-center">
           <Image
             src="/images/skirtTrans.svg"
             alt="About us"
@@ -149,7 +149,7 @@ const Page = () => {
       </section>
       <section>
         <div className="w-full bg-smokeGrey  flex flex-col items-center justify-center">
-          <h3 className="font-poppins font-bold text-stoneDark pt-4 text-center text-5xl">
+          <h3 className="font-poppins font-bold text-stoneDark md:pt-4 pt-8 text-center text-5xl">
             FASHION FOR A BETTER FUTURE
           </h3>
           <Image
