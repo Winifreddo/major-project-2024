9e2c5ff2ee941b7512170b92c624c7c33741ed9b
Leigh Richards <73237857+Winifreddo@users.noreply.github.com>
2024-07-03 21:13:56 +0100

    Update auth.ts

--- app/auth.ts
+++ app/auth.ts
@@ -2,7 +2,6 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 import Google from "next-auth/providers/google";
-import Credentials from "next-auth/providers/credentials"
 import { Adapter } from "next-auth/adapters";
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
