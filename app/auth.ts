fdb7cbdf674b4ff1bdbd7adf9e619cd338b69957
Leigh Richards <73237857+Winifreddo@users.noreply.github.com>
2024-07-03 20:35:36 +0100

    update auth.ts

--- app/auth.ts
+++ app/auth.ts
@@ -1,16 +1,36 @@
-import NextAuth from "next-auth"
+// import NextAuth from "next-auth"
+// import { PrismaAdapter } from "@auth/prisma-adapter";
+// import prisma from "@/lib/prisma";
+// import Google from "next-auth/providers/google";
+// import Credentials from "next-auth/providers/credentials"
+// import { Adapter } from "next-auth/adapters";
+ 
+// export const { handlers, signIn, signOut, auth } = NextAuth({
+
+//     theme: {
+//         brandColor: "#F5F0EC",
+//         logo: "/images/Reform.png",
+//     },
+//     adapter : PrismaAdapter(prisma) as Adapter,
+//   providers: [Google], 
+// })
+
+import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 import Google from "next-auth/providers/google";
-import Credentials from "next-auth/providers/credentials"
 import { Adapter } from "next-auth/adapters";
- 
-export const { handlers, signIn, signOut, auth } = NextAuth({
 
-    theme: {
-        brandColor: "#F5F0EC",
-        logo: "/images/Reform.png",
-    },
-    adapter : PrismaAdapter(prisma) as Adapter,
-  providers: [Google], 
-})
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  theme: {
+    brandColor: "#F5F0EC",
+    logo: "/images/Reform.png",
+  },
+  adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [
+      Google({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      }),
+    ],
+});
